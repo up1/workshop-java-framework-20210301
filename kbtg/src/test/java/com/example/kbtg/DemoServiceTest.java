3b5854a68a423e3b813107579d413c142d513326
up1 <somkiat.p@gmail.com>
2021-03-01 15:02:38 +0700

    Testing with exception

--- kbtg/src/test/java/com/example/kbtg/DemoServiceTest.java
+++ kbtg/src/test/java/com/example/kbtg/DemoServiceTest.java
@@ -13,16 +13,45 @@ public class DemoServiceTest {
     @DisplayName("ในการทำงานต้อง random ได้ค่า 5")
     public void random_5() {
         DemoService demoService = new DemoService();
-        demoService.setRandom(new Random5());
+        demoService.setRandom(new MockRandom(5));
         String actualResult =demoService.generateData("somkiat");
         assertEquals("somkiat5", actualResult);
     }
 
+    @Test
+    public void throw_exception_1() {
+        DemoService demoService = new DemoService();
+        demoService.setRandom(new MockRandom(1));
+        try {
+            demoService.generateData("somkiat");
+            fail();
+        }catch (RuntimeException e) {
+            assertEquals("Invalid number with 1", e.getMessage());
+        }
+    }
+
+    @Test
+    public void throw_exception_with_juit5() {
+        DemoService demoService = new DemoService();
+        demoService.setRandom(new MockRandom(1));
+        // JUnit 5 style
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            demoService.generateData("somkiat");
+        });
+        assertEquals("Invalid number with 1", exception.getMessage());
+    }
+
 }
 
-class Random5 extends Random {
+class MockRandom extends Random {
+    private final int result;
+
+    MockRandom(int result) {
+        this.result = result;
+    }
+
     @Override
     public int nextInt(int bound) {
-        return 5;
+        return result;
     }
 }
