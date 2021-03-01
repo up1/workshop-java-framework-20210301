abd11479823618fb9f40dfcb0379d423cdf02ed0
up1 <somkiat.p@gmail.com>
2021-03-01 11:51:13 +0700

    Show all beans from spring's context

--- kbtg/src/main/java/com/example/kbtg/KbtgApplication.java
+++ kbtg/src/main/java/com/example/kbtg/KbtgApplication.java
@@ -2,12 +2,24 @@ package com.example.kbtg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static java.lang.System.out;
 
 @SpringBootApplication
 public class KbtgApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KbtgApplication.class, args);
+		// IoC Container, Bean  => Spring
+		// DI (Dependency Injection)
+		// IoC (Inversion of Control)
+		ConfigurableApplicationContext context
+				= SpringApplication.run(KbtgApplication.class, args);
+		String[] beans = context.getBeanDefinitionNames();
+		for (String bean : beans) {
+			out.println(bean);
+		}
+		out.println(context.getBeanDefinitionCount());
 	}
 
 }
