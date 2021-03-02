4c76dbfcf194ca657ffb7e8526223dc8e50bb820
up1 <somkiat.p@gmail.com>
2021-03-02 11:19:11 +0700

    Working with lombok

--- kbtg/src/main/java/com/example/kbtg/user/UserResponse.java
+++ kbtg/src/main/java/com/example/kbtg/user/UserResponse.java
@@ -1,55 +1,15 @@
 package com.example.kbtg.user;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.commons.lang3.builder.EqualsExclude;
 
-public class UserResponse extends Object {
-    private Integer id;
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode
+public class UserResponse {
+    private int id;
     private String name;
-    private Integer age;
-
-    public UserResponse() {
-    }
-
-    public UserResponse(Integer id, String name, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserResponse that = (UserResponse) o;
-        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(age, that.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
+    private int age;
 }
