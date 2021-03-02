c548ca6b6ef4e8042c4f3f935753f37d3e326088
up1 <somkiat.p@gmail.com>
2021-03-02 17:28:56 +0700

    Success cases

--- kbtg/src/test/java/com/example/kbtg/post/PostGatewayTest.java
+++ kbtg/src/test/java/com/example/kbtg/post/PostGatewayTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -15,12 +17,16 @@ public class PostGatewayTest {
 
     @Test
     public void success_with_id_1() {
-
+        Optional<PostResponse> result = postGateway.getPostById(1);
+        assertTrue(result.isPresent());
+        PostResponse postResponse = result.get();
+        assertEquals(1, postResponse.getId());
+        assertEquals("sunt aut facere repellat provident occaecati excepturi optio reprehenderit", postResponse.getTitle());
     }
 
     @Test
     public void should_return_empty_when_exception_is_occured() {
-
+        // TODO
     }
 
 }
