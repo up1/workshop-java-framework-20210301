69fe0c27dd2c96ee9f37881a4f8daefcb96cf304
up1 <somkiat.p@gmail.com>
2021-03-02 15:42:44 +0700

    Unit test for service layer

--- kbtg/src/main/java/com/example/kbtg/user/UserService.java
+++ kbtg/src/main/java/com/example/kbtg/user/UserService.java
@@ -8,11 +8,16 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
+
     private UserRepository userRepository;
 
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public UserResponse getInfo(int id) {
-        Optional<MyUser> user = userRepository.findById(1);
+        Optional<MyUser> user = userRepository.findById(id);
         if(user.isPresent()) {
             // Success
             return new UserResponse(id, user.get().getName(), user.get().getAge());
---
+++ kbtg/src/test/java/com/example/kbtg/user/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.kbtg.user;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    public void found_user_by_id_1() {
+        // Arrange
+        MyUser somkiat = new MyUser();
+        somkiat.setId(1);
+        somkiat.setName("somkiat");
+        somkiat.setAge(30);
+        when(userRepository.findById(1)).thenReturn(Optional.of(somkiat));
+
+        UserService userService = new UserService(userRepository);
+        UserResponse response = userService.getInfo(1);
+        // Assert
+        assertEquals(1, response.getId());
+        assertEquals("somkiat", response.getName());
+        assertEquals(30, response.getAge());
+    }
+
+}
