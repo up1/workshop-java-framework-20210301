26d9e96f76e3753979dd48029f4c44205b754fac
up1 <somkiat.p@gmail.com>
2021-03-02 08:54:28 +0700

    No need

--- demo_activemq/src/main/java/com/example/demo_activemq/DemoActivemqApplication.java
+++ demo_activemq/src/main/java/com/example/demo_activemq/DemoActivemqApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
 
-@EnableJms
+//@EnableJms
 @SpringBootApplication
 public class DemoActivemqApplication {
 
